edaf00448ae9ed1d53cc84cb673a7d73217344e5
HANCORE-linux <230438592+HANCORE-linux@users.noreply.github.com>
2025-10-27 20:00:48 +0100

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -8,23 +8,23 @@ return {
             colors = {
                 -- Monotone shades (base00-base07)
                 base00 = "#d0d0c8", -- Default background
-                base01 = "#494a42", -- Lighter background (status bars)
-                base02 = "#d0d0c8", -- Selection background
-                base03 = "#494a42", -- Comments, invisibles
-                base04 = "#383835", -- Dark foreground
+                base01 = "#63452c", -- Lighter background (status bars)
+                base02 = "#2ec27e", -- Selection background
+                base03 = "#63452c", -- Comments, invisibles
+                base04 = "#576ddb", -- Dark foreground
                 base05 = "#242424", -- Default foreground
-                base06 = "#242424", -- Light foreground
-                base07 = "#383835", -- Light background
+                base06 = "#2a9292", -- Light foreground
+                base07 = "#576ddb", -- Light background
 
                 -- Accent colors (base08-base0F)
                 base08 = "#db0030", -- Variables, errors, red
                 base09 = "#ef597a", -- Integers, constants, orange
                 base0A = "#51573b", -- Classes, types, yellow
-                base0B = "#2e3125", -- Strings, green
-                base0C = "#363a34", -- Support, regex, cyan
+                base0B = "#383835", -- Strings, green
+                base0C = "#955ae7", -- Support, regex, cyan
                 base0D = "#f05a0f", -- Functions, keywords, blue
-                base0E = "#43432b", -- Keywords, storage, magenta
-                base0F = "#a2a87c", -- Deprecated, brown/yellow
+                base0E = "#bf40bf", -- Keywords, storage, magenta
+                base0F = "#ffa348", -- Deprecated, brown/yellow
             },
         },
         config = function(_, opts)
