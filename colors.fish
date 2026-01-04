6d73e5b2f9a5a99bf5f236957da7f83d9cf3492a
HANCORE-linux <m.fattah-ahmad@gmx.de>
2026-01-04 10:57:33 +0100

    add theme-hook-files
    added missing theme-hook-files

---
+++ colors.fish
@@ -0,0 +1,27 @@
+set -U background '#d0d0c8'
+set -U foreground '#242424'
+set -U cursor '#242424'
+set -U color0 '#d0d0c8'
+set -U color1 '#db0030'
+set -U color2 '#2e3125'
+set -U color3 '#51573b'
+set -U color4 '#DE6A41'
+set -U color5 '#43432b'
+set -U color6 '#363a34'
+set -U color7 '#383835'
+set -U color8 '#494a42'
+set -U color9 '#ef597a'
+set -U color10 '#7a846a'
+set -U color11 '#a2a87c'
+set -U color12 '#e88b5f'
+set -U color13 '#a8a67b'
+set -U color14 '#777b6f'
+set -U color15 '#242424'
+
+set -U fish_color_normal normal
+set -U fish_color_command green
+set -U fish_color_param cyan
+set -U fish_pager_color_completion
+set -U fish_pager_color_description $fish_color_quote yellow
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_history_current --bold
